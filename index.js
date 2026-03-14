10ea02a8bdb0040d94648ff2ec87f09bb74dfb27
arrowbow <763742875@qq.com>
2026-03-14 22:10:17 +0800

    1

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+            document.getElementsByClassName('button-1').onclick = function() {
+                console.log('Button clicked!');
+            };
+        });
