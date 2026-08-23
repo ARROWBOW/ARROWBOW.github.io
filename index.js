15acfff15c9346fb1820f647f4e14acb687f5594
ARROWBOW <763742875@qq.com>
2026-08-23 13:46:37 +0800

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-            document.getElementsByClassName('button-1').onclick = function() {
-                console.log('Button clicked!');
-            };
-        });
